19ea44f8781532597f14e3717e0e60b30b0664aa
Davy <davyfungaloi@gmail.com>
2021-04-08 21:01:47 +0200

    added axios & basic unit testing

--- functions.js
+++ functions.js
@@ -1,8 +1,12 @@
 const functions = {
-    add: (num1, num2) => num1 + num2,
-    add: function(num1, num2) {
-        return num1 +num2
-    }
-}
+  add: (num1, num2) => num1 + num2,
+  isNull: () => null,
+  checkValue: (x) => x,
+  createUser: () => {
+      const user = {firstName: 'Brad'}
+      user['lastName'] = "Traversy"
+      return user
+  }
+};
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -1,11 +1,52 @@
-const functions = require ("./functions")
+const functions = require("./functions");
 
-test('Adds 2+ 2 to equal 4', () => {
-    expect(functions.add(2,2)).toBe(4)
+test("Adds 2+ 2 to equal 4", () => {
+  expect(functions.add(2, 2)).toBe(4);
+});
 
+test("Adds 2+ 2 to equal 5", () => {
+  expect(functions.add(2, 2)).not.toBe(5);
+});
+
+// CHECK FOR TRUTHY & FALSY VALUES
+// toBeNull matches only null
+// toBeUndefined matches only undefined
+// toBeDefined is the opposite of toBeUndefined
+// toBeTruthy matches anything that an if statement treats as true
+// toBeFalsy matches anything that an if statement treats as false
+
+// To be null
+test("Should be null", () => {
+  expect(functions.isNull()).toBeNull();
+});
+
+// To be falsy
+test("Should be falsy", () => {
+  expect(functions.checkValue(0)).toBeFalsy();
+});
+
+///To Equal for Objects
+test("User should be Brad Travery Object", () => {
+  expect(functions.createUser()).toEqual({
+    firstName: "Brad",
+    lastName: "Traversy",
+  });
+});
+
+/// Less than and greater than
+test('Should be under 1600', () => {
+    const load1=800
+    const load2=800
+    expect(load1+load2).toBeLessThanOrEqual(1600)
 })
 
-test('Adds 2+ 2 to equal 5', () => {
-    expect(functions.add(2,2)).not.toBe(5)
+//regex
 
+test('There is no I in team', () => {
+    expect('team').not.toMatch(/I/i)
+})
+//Arrays contain
+test('admin should be in usernames', () => {
+    usernames=['john', 'karen', 'admin']
+    expect(usernames).toContain('admin')
 })
